6ca9f9947afb0c9580a4e3cba3c9964bd0976ead
blakehays17 <98183066+blakehays17@users.noreply.github.com>
2022-12-14 01:22:52 -0600

    Update header.php

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
                     <a class="nav-link" href="enrolls.php">Enrolls</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="unenroll.php">Unenroll</a>
+                    <a class="nav-link" href="change_course.php">Change Course</a>
                 </li>
             </ul>
         </div>
