7d8f08fd8f5f7303edce6fa674f3ca8e7689855a
blakehays17 <98183066+blakehays17@users.noreply.github.com>
2022-12-14 02:52:11 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include("bootstrap.php"); ?>
+    <link rel="stylesheet" href="main.css">
+    <title>Homework 4</title>
+</head>
+
+<body>
+    <?php include("header.php"); ?>
+    <div class="content container">
+        <h1>Homework 4</h1>
+        <br>
+        <h2>Instructors</h2>
+        <p>
+            On the instructors page you can edit an instructor's first name, add an instructor, or delete one.
+        </p>
+        <br>
+        <h2>Courses</h2>
+        <p>
+            On the courses page you can view each course ID and subject. You can also manipulate the data if you'd like. 
+        </p>
+        <br>
+        <h2>Students</h2>
+        <p>
+            The students page consists of the student ID and student name. If you'd like, you can chnage a student's name, delete them, or add a new student. 
+        </p>
+        <br>
+        <h2>Enrolls</h2>
+        <p>
+            On the enrolls page you can change, add, or delete a semester for a given enrollment ID.
+        </p>
+        <h2>Change Course</h2>
+        <p>
+            On the change course page you can change which course a student is in incase there is an error.
+        </p>
+    </div>
+    <?php include("footer.php"); ?>
+</body>
+
+</html>
